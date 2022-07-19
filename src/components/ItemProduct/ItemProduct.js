bf94563619f51cc1cc756271ac96f5aa916bb769
Lucia Pereda <luciapereda21@gmail.com>
2022-07-19 20:59:21 -0300

    entrega 4

--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,15 +1,36 @@
 
 import  './ItemProduct.css'
+import { useState,useEffect } from 'react';
 
 
-const ItemProduct = ({title,price,image}) =>{
+const ItemProduct = ({title,price,image,stock,action}) =>{
+    const [contador, setContador] = useState(0)
+   
+    const addNumber = () =>{
+        if(contador<stock)
+        setContador(contador + 1)
+    }
+    const restNumber = () =>{
+        setContador(contador - 1)
+    }
+    useEffect(()=>{
+        // fetch().then(
+            // setProduct()
+        // )
+ 
+     },[contador])
     return (
         <div className="product-card">
                 <img src={`/assets/icons/images/${image}`}  alt=""  />
                 <div className='product-info'>
                 <p>{title}</p>
                 <span>${price}</span>
-                </div>               
+                </div> 
+                <div className='contador'>
+                    <button onClick={restNumber}>-</button>
+                    <p>{contador}</p>
+                    <button onClick={addNumber}>+</button>
+                </div>              
                  <button>Comprar</button>
         </div>
     )
--- src/components/ItemProductContainer/ItemProductContainer.js
+++ src/components/ItemProductContainer/ItemProductContainer.js
@@ -1,17 +1,25 @@
 import './ItemProductContainer.css'
 import ItemProduct from '../ItemProduct/ItemProduct'
+import { useEffect } from 'react'
 function ItemProductContainer ({section}) {
+    useEffect(()=>{
+       // fetch().then(
+           // setProduct()
+       // )
+
+    },[])
+    // si dejo el array del useeffect vacio se va a ejecutar solo al montar el componente y si no le paso el array se va a ejecutar en una actualizacion
     return(
     <div className='main-container'>
        <h1 className='titulo-container'>
             {section}
         </h1>
          <div className='products-container'>
-             <ItemProduct title="Bananas" price="420" image={'banabas.jpg'}></ItemProduct>
-             <ItemProduct title="Peras" price="500" image={'peras.jpg'}></ItemProduct>
-             <ItemProduct title="Uvas" price="340" image={'uvas.jpg'}></ItemProduct>
-             <ItemProduct title="Manzanas" price ="470" image={'manzanas.jpg'} />
-             <ItemProduct title="Peras" price="500" image={'peras.jpg'}></ItemProduct>
+             <ItemProduct title="Bananas" price="420" image={'banabas.jpg'} stock={10}></ItemProduct>
+             <ItemProduct title="Peras" price="500" image={'peras.jpg'} stock={15}></ItemProduct>
+             <ItemProduct title="Uvas" price="340" image={'uvas.jpg'} stock={2}></ItemProduct>
+             <ItemProduct title="Manzanas" price ="470" image={'manzanas.jpg'} stock={6} />
+             <ItemProduct title="Peras" price="500" image={'peras.jpg'} stock={20}></ItemProduct>
 
       </div>
     </div>
